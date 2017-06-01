a5a7ecf9c6565cc50c3a8c38780ee95351d6b3d1
caroquintana <caro.quintana.n@gmail.com>
2017-06-01 02:36:16 -0400

    js ok

--- js/app.js
+++ js/app.js
@@ -1,41 +1,43 @@
 /*
  * Archivo principal de funcionalidad de JS
 
- var imagenes = document.getElementById('galeria').getElementsByTagName('img');
+
+(function modal(){
+  var traerGaleria = Array.from(document.getElementsByClassName("col-4"));
+  var modal = document.getElementById("cajamodal");
 
 
- for( var i = 0; i < imagenes.length; i++){
+    function crearNuevos(){
+          modal.innerHTML = "";
+          bodyModal = document.createElement("div");
+          bodyModal.classList.add("modal-body");
+          bodyModal.innerHTML = box.innerHTML;
+          modal.appendChild(bodyModal);
+          modal.classList.remove("hide");
 
- 	imagenes[i].onClick
- }
+          close = document.createElement("div");
+          close.classList.add("close");
 
-  (function(){
-    var traerGaleria = document.querySelector('.row'),
-        cierre = document.querySelector('.cierre'),
-        modalImage = document.querySelector('.slider img');
+          img = document.createElement("img");
+          img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
 
-    traerGaleria.addEventListener('click', openFullSize, false);
-  
-  function openFullSize(e){
-      var src = traerGaleria.firstElementChild.src;
-      var boton = document.querySelector('.exit');
-          modalOpen = true;
+          close.appendChild(img);
+          modal.appendChild(close);
+      };
+
+    function ocultar(){
+        modal.classList.add("hide");
+      };
     
-      modalImage.src = src;
-    
-      if(modalOpen){
-        cierre.className = 'modal-open';
-        boton.addEventListener('click', function(){
-          cierre.className = 'modal-closed';
-        })
-      }
-    e.stopPropagation();
-};
+  var bodyModal, close, img;
+
+  traerGaleria.forEach(function(box){
+    box.addEventListener("click", crearNuevos); 
+    close.addEventListener("click", ocultar); 
+  });
 
 })();
-
-
- */
+  */
 
 (function modal(){
   var traerGaleria = Array.from(document.getElementsByClassName("col-4"));
@@ -44,6 +46,8 @@
   
 
   var bodyModal, close, img;
+  
+
   traerGaleria.forEach(function(box){
     box.addEventListener("click", function(){
       modal.innerHTML = "";
@@ -68,4 +72,4 @@
   });
 })();
 
- 
+
