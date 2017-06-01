9f5a94e561d25ad4d663da331a2487fe3ed0500f
caroquintana <caro.quintana.n@gmail.com>
2017-06-01 02:22:55 -0400

    css y js elementos nuevos

--- js/app.js
+++ js/app.js
@@ -38,18 +38,34 @@
  */
 
 (function modal(){
-  var traerGaleria = Array.from(document.getElementsByClassName("img-galeria"));
+  var traerGaleria = Array.from(document.getElementsByClassName("col-4"));
   var modal = document.getElementById("cajamodal");
 
+  
+
   var bodyModal, close, img;
-  traerGaleria.forEach(function(galeria){
+  traerGaleria.forEach(function(box){
     box.addEventListener("click", function(){
       modal.innerHTML = "";
       bodyModal = document.createElement("div");
       bodyModal.classList.add("modal-body");
       bodyModal.innerHTML = box.innerHTML;
-    })
-  })
-})
+      modal.appendChild(bodyModal);
+      modal.classList.remove("hide");
+
+      close = document.createElement("div");
+      close.classList.add("close");
+
+      img = document.createElement("img");
+      img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+
+      close.appendChild(img);
+      modal.appendChild(close);
+      close.addEventListener("click", function(){
+        modal.classList.add("hide");
+      });
+    });
+  });
+})();
 
  
