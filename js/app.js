4b6ef47a0686e9d165ceee50b99740516f138104
caroquintana <caro.quintana.n@gmail.com>
2017-06-01 00:44:22 -0400

    avances js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
 /*
  * Archivo principal de funcionalidad de JS
+
+ var imagenes = document.getElementById('galeria').getElementsByTagName('img');
+
+
+ for( var i = 0; i < imagenes.length; i++){
+
+ 	imagenes[i].onClick
+ }
+
+  (function(){
+    var traerGaleria = document.querySelector('.row'),
+        cierre = document.querySelector('.cierre'),
+        modalImage = document.querySelector('.slider img');
+
+    traerGaleria.addEventListener('click', openFullSize, false);
+  
+  function openFullSize(e){
+      var src = traerGaleria.firstElementChild.src;
+      var boton = document.querySelector('.exit');
+          modalOpen = true;
+    
+      modalImage.src = src;
+    
+      if(modalOpen){
+        cierre.className = 'modal-open';
+        boton.addEventListener('click', function(){
+          cierre.className = 'modal-closed';
+        })
+      }
+    e.stopPropagation();
+};
+
+})();
+
+
  */
+
+(function modal(){
+  var traerGaleria =
+})
+
+ 
